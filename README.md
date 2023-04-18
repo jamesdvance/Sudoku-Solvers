57bcb79969d14e735b7157392e73e59ad01ecbdf
jamesdvance <james.dvance@gmail.com>
2023-04-17 21:36:36 -0400

    added working rl with stable baselines

--- README.md
+++ README.md
@@ -25,9 +25,20 @@ Solving Sukoku is a popular Leetcode [problem](https://leetcode.com/problems/sud
 ### Reinforcement Learning
 
 
+### Supervised Learning
+
+
 ## References
 
 * [Sudoku As Graph Coloring Problem](https://www.linkedin.com/pulse/solve-your-sudoku-graph-coloring-problem-alireza-soroudi/?trk=eml-email_series_follow_newsletter_01-hero-1-title_link&midToken=AQET4HYp_zAXXw&fromEmail=fromEmail&ut=2RbmToAhecnGI1)
 * [Sudoku As A Linear Program](https://www.coin-or.org/PuLP/CaseStudies/a_sudoku_problem.html)
 * [Sugoku Puzzle API](https://github.com/bertoort/sugoku)
 * [Sudoku-Gym](https://github.com/wcheung-code/sudoku-gym)
+* [Reinforcement Learning For Constraint Satisfaction Game Agents](https://arxiv.org/ftp/arxiv/papers/2102/2102.06019.pdf)
+* [One Million Sudoku Games Dataset](https://www.kaggle.com/datasets/bryanpark/sudoku)
+* [Git Repo That Spawned Million Games Dataset](https://github.com/Kyubyong/sudoku)
+* [Supervised Learning with CNNs (99% accuracy)](https://github.com/shivaverma/Sudoku-Solver/blob/master/sudoku.ipynb)
+* [Deep Q W/ CNNs](https://cse.iitkgp.ac.in/~sudeshna/courses/DL18/CNN-DeepQ-8Mar-2018.pdf)
+* [OptNet: Differentiable Optimization As A Layer In Neural Networks](http://proceedings.mlr.press/v70/amos17a/amos17a.pdf)
+* [Solving Sudoku With Neural Networks](https://cs230.stanford.edu/files_winter_2018/projects/6939771.pdf)
+* [Deep Q with CNN's / Pytorch](https://towardsdatascience.com/deep-q-network-with-pytorch-146bfa939dfe)
---
+++ reinforcement_learning/env/sudoku.py
@@ -0,0 +1,202 @@
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+import sys
+
+
+resolved = 0
+unfinished = 1
+error = 2
+
+
+# Check a solution is correct by checking the 3 contraints on all digits
+#	- digit is unique in row
+#	- digit is unique in column
+#	- digit is unique in square
+#  @return
+#	- resolved if the grid is resolved
+#	- unfinished if the grid is not yet finished
+#	- error if one of the contraints is not respected
+def checkSolution(grid):
+	N = len(grid)
+
+	for i in range(N):
+		for j in range(N):
+			# If a case is not filled, the sudoku is not finished
+			if grid[i][j] == 0:
+				return unfinished
+
+			n = N//3
+			iOffset = i//n*n
+			jOffset = j//n*n
+			square = grid[ iOffset:iOffset + n , jOffset:jOffset + n].flatten()
+			# Check uniqueness
+			uniqueInRow    = countItem(grid[i], grid[i, j])  == 1
+			uniqueInCol    = countItem(grid[:,j:j+1].flatten(), grid[i, j]) == 1
+			uniqueInSquare = countItem(square, grid[i, j]) == 1
+
+			if not (uniqueInRow and uniqueInCol and uniqueInSquare):
+				return error
+
+	return resolved
+
+
+# Count the number of time the item appears in a vector
+def countItem(vector, item):
+	count = 0
+	for item2 in vector:
+		if item2 == item: count += 1
+	return count
+
+
+# Recursivly find all solutions (backtracking)
+# @param stopAt make the backtracking stop when it found x solutions
+# @param i, j force to start the backtracking from the case (i, j)
+# @param omit prevent looking into a possibility
+def getSolutions(grid, stopAt=1, i=-1, j=-1, omit=-1):
+	N = len(grid)
+	check = checkSolution(grid)
+	# Check if grid is resolve or if there is an error
+	if check == resolved:
+		return np.array([grid], dtype=int)
+	if check == error:
+		return np.empty(shape=(0,N,N), dtype=int)
+
+	# If i and j are not setted, get the first empty spot and start backtracking from it
+	if i == -1:
+		for i in range(N):
+			for j in range(N):
+				# If not empty spot continue
+				if grid[i, j] == 0: break
+			if grid[i, j] == 0: break
+
+	# Randomize possible values
+	values = np.arange(1, N+1)
+	np.random.shuffle(values)
+	# Try all possiblities from those values until we reach the max nb of solutions asked by stopAt
+	solutions = np.empty(shape=(0,N,N), dtype=int)
+	for value in values:
+		if omit == value: continue
+		cGrid = np.copy(grid)
+		cGrid[i, j] = value
+		subSolutions = getSolutions(cGrid, stopAt=stopAt-len(solutions))
+		solutions = np.concatenate((solutions, subSolutions))
+		if len(solutions) >= stopAt:
+			return solutions
+	return solutions
+
+
+class SudokuEnv(gym.Env):
+	metadata = {'render.modes': ['human']}
+	last_action = None
+
+	# Make a random grid and store it in self.base
+	def __init__(self):
+		# The box space is continuous. This don't apply to a sudoku grid, but there is no other choices
+		self.observation_space = spaces.Box(low=1, high=9, shape=(9, 9))
+		self.action_space = spaces.Discrete(729) # 9*9*9
+		action_choices = [(i,j,k) for k in range(9) for j in range(9) for i in range(9)]
+		self.action_to_
+		# Get a random solution for an empty grid
+		self.grid = []
+		self.base = getSolutions(np.zeros(shape=(9,9)))[0]
+		# Get all positions in random order, to randomly parse the grid
+		N = len(self.base)
+		positions = []
+		for i in range(N):
+			for j in range(N):
+				positions.append((i, j))
+		np.random.shuffle(positions)
+
+		count = 0
+		# Try to put 0 instead of the original value for all positions
+		# Stop after 40 --> medium difficulty
+		# This is slow after 40 because, the algorithm looks for 1 solutions when there is none,
+		# so it realy check all the possibilities...
+		for i, j in positions:
+			if count > 40:
+				break
+			oldValue = self.base[i, j]
+			self.base[i, j] = 0
+			solutions = getSolutions(self.base, stopAt=2, i=i, j=j, omit=oldValue)
+			if len(solutions) == 0:
+				count += 1
+			else:
+				# if more than one solution undo
+				self.base[i, j] = oldValue
+
+
+	# @return
+	# 	- a copy of the grid to prevent alteration from the user
+	# 	- a reward: - negative if action leads to an error
+	#	            - positive if action is correct or grid is resolved
+	def step(self, action:int):
+		self.last_action = action
+		oldGrid = np.copy(self.grid)
+
+		# The user can't replace a value that was already set
+        
+        
+		if self.grid[action[0], action[1]] != 0:
+			return np.copy(self.grid), -1, False, {}
+
+		# We add one to the action because the action space is from 0-8 and we want a value in 1-9
+		self.grid[action[0], action[1]] = action[2]+1
+
+		stats = checkSolution(self.grid)
+		# If grid is complet or correct, return positive reward
+		if stats == resolved:
+			return np.copy(self.grid), 1, True, {}
+		elif stats == unfinished:
+			return np.copy(self.grid), 1, False, {}
+		if stats == error:
+			# If move is wrong, return to old state, and return negative reward
+			self.grid = oldGrid
+			return np.copy(self.grid), -1, False, {}
+
+
+	# Replace self.grid with self.base
+	# Creating a new grid at every reste would be expensive
+	def reset(self):
+		self.last_action = None
+		self.grid = np.copy(self.base)
+		return np.copy(self.grid)
+
+
+	def render(self, mode='human', close=False):
+
+		for i in range(len(self.grid)):
+			for j in range(len(self.grid)):
+				if self.last_action != None and i == self.last_action[0] and j == self.last_action[1]:
+					if self.last_action[2] == self.grid[i, j]:
+						sys.stdout.write('\033[92m' + str(self.last_action[2]) + '\033[0m')
+					else:
+						sys.stdout.write('\033[91m' + str(self.last_action[2]) + '\033[0m')
+				else:
+					sys.stdout.write(str(self.grid[i, j]))
+				if j % 3 == 2 and j != len(self.grid)-1:
+					sys.stdout.write(' | ')
+			if i % 3 == 2 and i != len(self.grid)-1:
+				sys.stdout.write('\n---------------\n')
+			else:
+				sys.stdout.write('\n')
+		sys.stdout.write('\n\n')
+		sys.stdout.flush()
+
+
+# env = SudokuEnv()
+# env._reset()
+# print env.grid
+
+# grid = np.array(
+# [[0,0,0,4,0,9,0,0,1],
+# [0,0,4,0,3,0,0,2,0],
+# [0,7,2,0,5,1,0,0,6],
+# [4,2,1,0,0,5,6,0,0],
+# [8,0,0,0,0,2,0,0,0],
+# [3,0,0,9,0,0,0,0,0],
+# [0,1,0,5,7,4,0,0,0],
+# [5,0,6,0,0,3,0,0,7],
+# [0,0,3,0,9,0,0,1,0]])
+#
+# print getSolutions(grid)
